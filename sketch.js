2b36b34de9448276d391594317f09ed6070c251b
harkiran7237 <harkiran7237@gmail.com>
2020-05-11 14:49:52 +0530

    all in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,90 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-var block;
-var bob1;
-var chain1;
+var engine, world;
+var holder,ball,ground;
 
-function setup(){
-  var canvas = createCanvas(400,400);
+function setup() {
+  createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
 
-  block= new pendulum(150,50,200,25);
-  bob1= new bob(35);
-  chain1=new chain(block.body,bob1.body);
+  var ground_options={
+    isStatic : true
+  }
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options)
+  World .add(world,ground);
+
+holder = Bodies.rectangle(200,100,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
+}
+
+ball  = Bodies.circle(220,200,40,ball_options);
+World.add(world,ball);
+
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.004,
+  length : 100
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("WHite");
 }
 
 
-  function draw(){
-    background("yellow");
-    Engine.update(engine);
+function draw() {
+  background("yellow"); 
+  Engine.update(engine);
+fill("blue");
+text("Press SPACE to move the pendulum",10,20);
+text("Press ENTER to release mouseOver",10,40);
 
-   if(keyCode===32){
-    
-    bob1.display();
-   }
-   else if(keyCode===13){
-     bob1.display1();
-   }
-  
-    block.display();
-    
-    
-    
-  
-  
-  
-  }
+  fill ("brown");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+fill("blue");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(8);
+stroke("white");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+
+
+
+if(keyCode===32){
+ball.position.y = mouseY;
+ball.position.x = mouseX;
+}
+
+else if (keyCode === 13){
+ball.position.x = 200;
+
+}
+
+}
